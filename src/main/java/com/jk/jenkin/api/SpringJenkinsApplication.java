a2fff8ab13ddf3356e8558d7868d00c6319da461
Surendar M <surendar.m@publicissapient.com>
2024-02-11 22:00:50 +0530

    first

---
+++ src/main/java/com/jk/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.jk.jenkin.api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
